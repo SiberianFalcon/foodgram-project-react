69b84806a767c7b665f04e72e3414fa4b3a65e50
SiberianFalcon <bq.Sokol@yandex.ru>
2023-09-19 13:52:38 +0300

    authentication done

--- backend_foodgram/api/admin.py
+++ backend_foodgram/api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User
+
+
+from api.models import User
 
 
 admin.site.register(User)
--- backend_foodgram/api/models.py
+++ backend_foodgram/api/models.py
@@ -1,21 +1,20 @@
-from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
-    first_name = models.CharField(_("first name"), max_length=150, blank=False, null=False)
-    last_name = models.CharField(_("last name"), max_length=150, blank=False, null=False)
-    email = models.EmailField(_("email address"), max_length=150, blank=False, null=False, unique=False)
+    first_name = models.CharField(("first name"), max_length=150, blank=False, null=False)
+    last_name = models.CharField(("last name"), max_length=150, blank=False, null=False)
+    email = models.EmailField(("email address"), max_length=150, blank=False, null=False, unique=True)
     is_staff = models.BooleanField(
-        _("staff status"),
+        ("staff status"),
         default=False,
-        help_text=_("Designates whether the user can log into this admin site."),
+        help_text=("Designates whether the user can log into this admin site."),
     )
     is_active = models.BooleanField(
-        _("active"),
+        ("active"),
         default=True,
-        help_text=_(
+        help_text=(
             "Designates whether this user should be treated as active. "
             "Unselect this instead of deleting accounts."
         ),
@@ -25,36 +24,31 @@ class User(AbstractUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name", "username"]
 
-    class Meta:
-        verbose_name = _("user")
-        verbose_name_plural = _("users")
-        abstract = True
-
     
 
-class Tag(models.Model):
-    color = models.CharField(max_length=16)
-    name = models.CharField(max_length=64, null=False)
-    slug = models.SlugField(null=False)
+# class Tag(models.Model):
+#     color = models.CharField(max_length=16)
+#     name = models.CharField(max_length=64, null=False)
+#     slug = models.SlugField(null=False)
 
 
 
-class Ingridients(models.Model):
-    amount = models.PositiveSmallIntegerField()
-    name = models.CharField()
-    measurement_unit = models.CharField(max_length=10)
+# class Ingridients(models.Model):
+#     amount = models.PositiveSmallIntegerField()
+#     name = models.CharField()
+#     measurement_unit = models.CharField(max_length=10)
 
 
-class Recipe(models.Model):
-    author = models.ForeignKey(User, related_name="author", on_delete=models.CASCADE)
-    tag = models.ManyToManyField(Tag, related_name='tag')
-    text = models.TextField()
-    name = models.CharField(max_length=128, null=False)
-    ingridients = models.ManyToManyField(Ingridients)
-    image = models.ImageField()
-    cooking_time = models.CharField()
+# class Recipe(models.Model):
+#     author = models.ForeignKey(User, related_name="author", on_delete=models.CASCADE)
+#     tag = models.ManyToManyField(Tag, related_name='tag')
+#     text = models.TextField()
+#     name = models.CharField(max_length=128, null=False)
+#     ingridients = models.ManyToManyField(Ingridients)
+#     image = models.ImageField()
+#     cooking_time = models.CharField()
 
 
-class Subscription(models.Model):
-    name = models.ManyToManyField(User)
-    is_subscribed = models.BooleanField(default=False)
+# class Subscription(models.Model):
+#     name = models.ManyToManyField(User)
+#     is_subscribed = models.BooleanField(default=False)
