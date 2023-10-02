82a68b6d2a8c5787299bc45c7178ba1777a16834
SiberianFalcon <bq.Sokol@yandex.ru>
2023-10-02 12:07:15 +0300

    new way

---
+++ backend_foodgram/api/urls.py
@@ -0,0 +1,20 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import (CustomUserViewSet, IngredientViewSet, RecipeViewSet,
+                    TagViewSet)
+
+app_name = 'api'
+
+router = routers.DefaultRouter()
+
+router.register(r'tags', TagViewSet, basename='tag')
+router.register(r'ingredients', IngredientViewSet, basename='ingredient')
+router.register(r'recipes', RecipeViewSet, basename='recipe')
+router.register(r'users', CustomUserViewSet, basename='user')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken'))
+]
---
+++ backend_foodgram/core/tools.py
@@ -0,0 +1,58 @@
+import os
+
+from django.conf import settings
+from django.http import HttpResponse
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
+from rest_framework.generics import get_object_or_404
+
+from api.models import Recipe
+
+FONT_SIZE = 24
+HEIGHT = 750
+HEADER_LENGTH = 200
+HEADER_HEIGHT = 800
+TEXT_SIZE = 16
+LENGTH = 75
+
+
+def get_user_and_recipe_or_404(request, pk):
+    user = request.user
+    recipe = get_object_or_404(Recipe, id=pk)
+    return user, recipe
+
+
+def generate_ingredients_list_via_pdf(ingredient_list):
+    font_path = os.path.join(settings.FONT_ROOT, 'Zekton.ttf')
+    pdfmetrics.registerFont(
+        TTFont(name='Zekton', filename=font_path, validate='UTF-8'))
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = ('attachment; '
+                                       'filename="shopping_list.pdf"')
+    page = canvas.Canvas(response)
+    page.setFont('Zekton', size=FONT_SIZE)
+    page.drawString(HEADER_LENGTH, HEADER_HEIGHT, 'Список ингредиентов')
+    page.setFont('Zekton', size=TEXT_SIZE)
+    height = HEIGHT
+    for i, (name, data) in enumerate(ingredient_list.items(), 1):
+        page.drawString(LENGTH, height, (f'{i}. {name} - {data["amount"]}, '
+                                         f'{data["measurement_unit"]}'))
+        height -= 25
+    page.showPage()
+    page.save()
+    return response
+
+
+def form_ingredients_list(queryset):
+    ingredient_list = {}
+    for item in queryset:
+        name = item[0]
+        if name not in ingredient_list:
+            ingredient_list[name] = {
+                'measurement_unit': item[1],
+                'amount': item[2]
+            }
+        else:
+            ingredient_list[name]['amount'] += item[2]
+    return ingredient_list
