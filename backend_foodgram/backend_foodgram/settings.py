b10b1df26b38e3455d6bbba53d5aa775f9a8a45e
SiberianFalcon <bq.Sokol@yandex.ru>
2023-10-09 20:05:22 +0300

    need fix media

--- backend_foodgram/backend_foodgram/settings.py
+++ backend_foodgram/backend_foodgram/settings.py
@@ -75,23 +75,14 @@ WSGI_APPLICATION = 'backend_foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'food_db'),
+        'USER': os.getenv('POSTGRES_USER', 'food_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         # Меняем настройку Django: теперь для работы будет использоваться
-#         # бэкенд postgresql
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('POSTGRES_DB', 'food_db'),
-#         'USER': os.getenv('POSTGRES_USER', 'food_user'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-#         'HOST': os.getenv('DB_HOST', ''),
-#         'PORT': os.getenv('DB_PORT', 5432)
-#     }
-# }
 
 AUTH_USER_MODEL = 'user.CustomUser'
 
