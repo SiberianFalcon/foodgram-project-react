6230872786e80152e7ff6f97382b2982ebebdc70
SiberianFalcon <bq.Sokol@yandex.ru>
2023-10-12 11:08:27 +0300

    fix rtoubles with the pictures

--- backend_foodgram/backend_foodgram/settings.py
+++ backend_foodgram/backend_foodgram/settings.py
@@ -6,18 +6,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uy=o8j5y)haovbe-=nfb9(!gf7jnbn6ek8q7_&fnb_mltslqo4'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [os.getenv('HOST_1'), os.getenv('HOST_2'), os.getenv('HOST_3'), os.getenv('HOST_4')]
@@ -113,7 +105,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 FONT_ROOT = os.path.join(BASE_DIR, 'core')
 
