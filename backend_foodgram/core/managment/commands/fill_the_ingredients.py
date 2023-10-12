7cfbb49d32b5da49e5c566ba1cf8e949bef22f85
SiberianFalcon <bq.Sokol@yandex.ru>
2023-10-12 12:56:31 +0300

    csv

---
+++ backend_foodgram/core/managment/commands/fill_the_ingredients.py
@@ -0,0 +1,18 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from api.models import Ingredient
+from . import ingredients
+
+
+class Command(BaseCommand):
+
+    def add_ingredient_objects_in_database(self, *args, **options):
+        with open(ingredients, 'r', encoding="utf-8") as file:
+            csv_reader = csv.reader(file)
+            ingredient_list = [Ingredient(
+                name=name, measurement_unit=measurement_unit)
+                for name, measurement_unit in csv_reader]
+
+            Ingredient.objects.bulk_create(ingredient_list)
